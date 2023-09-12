5b4cf253b5b47d562e261480fcb8563bde09c01a
aaingyunii <dlsrbs98@gmail.com>
2023-09-12 17:59:14 +0900

    Update MovieRepository.java

--- movie_web/movie_recommendation_springboot/movie/src/main/java/com/example/movie/data/repository/MovieRepository.java
+++ movie_web/movie_recommendation_springboot/movie/src/main/java/com/example/movie/data/repository/MovieRepository.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.example.movie.data.entity.Movie;
 
-@Repository
 public interface MovieRepository extends JpaRepository<Movie, String>{
     @Query(value = "select * from movie m where m.title=:title limit 1", nativeQuery=true)
     public Movie findByTitle (@Param(value="title") String title);
